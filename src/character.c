16f7dc837abd0968aa0f26592b8ddc4f13568bb2
Vashli300 <vashli300@gmail.com>
2023-12-29 22:25:42 +1100

    Name selection

--- src/character.c
+++ src/character.c
@@ -21,18 +21,33 @@ Character createCharacter() {
     printf("> YOUR CHARACTER STARTS WITH:\n> STR %d\n> CON %d\n> INT %d\n> WIS %d\n> DEX %d\n> CHA %d\n", 
     newCharacter.ABILITY_STRENGTH,  newCharacter.ABILITY_CONSTITUTION, newCharacter.ABILITY_INTELLIGENCE, newCharacter.ABILITY_WISDOM, newCharacter.ABILITY_DEXTERITY,  newCharacter.ABILITY_CHARISMA);
 
-    chooseSpecies(newCharacter);
+    chooseSpecies();
 
-    chooseClass(newCharacter);
+    chooseClass();
     
     printf("> YOUR CHARACTER'S CLASS IS %s. GOOD CHOICE!\n", newCharacter.CLASS.NAME);
-    printf("> YOUR CHARACTER STARTS WITH %d HITPOINTS.\n", newCharacter.HITPOINTS_MAX);
+    printf("> THEY START WITH %d HITPOINTS.\n", newCharacter.HITPOINTS_MAX);
 
     printf("> YOU START WITH %d GOLD. CHOOSE SOME EQUIPMENT!\n", newCharacter.GOLD);
     
-    chooseArmour(newCharacter);
+    chooseArmour();
     
-    chooseOtherEquipment(newCharacter);
+    //chooseOtherEquipment(newCharacter);
+
+    chooseName();
 
     return newCharacter;
+}
+
+void chooseName() {
+    printf("> DON'T FORGET - YOUR HERO NEEDS A NAME!\n> ");
+    
+    char input[50];
+
+    fgets(input, sizeof(input), stdin);
+    sscanf(input, "%s", input);
+    strcpy(newCharacter.NAME, input);
+
+    printf("\n> FINISHED SETUP FOR %s.", newCharacter.NAME);
+
 }
