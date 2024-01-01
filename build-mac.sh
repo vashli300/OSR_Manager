e03f97f4b6884741ccd8fa63f6f1e1305efaa0cb
Vashli300 <vashli300@gmail.com>
2024-01-01 21:31:10 +1100

    Build script counter update and sucess message & cleanup

--- build-mac.sh
+++ build-mac.sh
@@ -1,2 +1,20 @@
 #!bin/bash
+
+#!/bin/bash
+
+counter_file="build_counter.txt"
+if [ ! -e "$counter_file" ]; then
+  echo 0 > "$counter_file"
+fi
+
+count=$(<"$counter_file")
+((count++))
+
+echo "Build Sucessful! "
+
+echo "Build number: $count"
+
+echo "$count" > "$counter_file"
+
 gcc -g -O3 -Wall -Wextra -pedantic -Wno-missing-braces src/main.c src/dice.c src/initmenu.c src/character.c src/equipment.c src/species-handler.c src/class-handler.c -o bin/build-mac
+
